c3580728b9da1611a28e2107bfbedc08ca63027b
leon-reisinger <kiliantolgamail.com>
2020-11-08 00:50:48 +0100

    Weapon weiter codiert

--- Streams.java
+++ Streams.java
@@ -7,46 +7,57 @@ import java.util.stream.Collectors;
 public class Streams {
 
     public double average(int[] numbers) {
+        return 0;
         //implement this
     }
     
     public List<String> upperCase(String[] strings) {
+        return null;
         //implement this
     }
     
     public Weapon findWeaponWithLowestDamage(List<Weapon> weapons) {
+        return null;
         //implement this
     }
     
     public Weapon findWeaponWithHighestStrength(List<Weapon> weapons) {
+        return null;
         //implement this
     }
     
     public List<Weapon> collectMissileWeapons(List<Weapon> weapons) {
+        return null;
         //implement this
     }
     
     public Weapon findWeaponWithLongestName(List<Weapon> weapons) {
+        return null;
         //implement this
     }
     
     public List<String> toNameList(List<Weapon> weapons) {
+        return null;
         //implement this
     }
     
     public int[] toSpeedArray(List<Weapon> weapons) {
+        return null;
         //implement this
     }
     
     public int sumUpValues(List<Weapon> weapons) {
+        return 0;
         //implement this
     }
     
     public long sumUpHashCodes(List<Weapon> weapons) {
+        return 0;
         //implement this
     }
     
     public List<Weapon> removeDuplicates(List<Weapon> weapons) {
+        return null;
         //implement this
     }
     
--- Weapon.java
+++ Weapon.java
@@ -1,6 +1,12 @@
 package net.htlgrieskirchen.pos3.streams;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Weapon {
     
@@ -12,55 +18,91 @@ public class Weapon {
     private final int minStrength;
     private int value;
 
-    public Weapon(String name, CombatType combatType, DamageType damageType, int damage, int speed, int minStrength, int value) {
-        //implement this
+    public Weapon(String name, CombatType combatType, DamageType damageType, int damage, int speed, int minStrength) {
+        this.name = name;
+        this.combatType = combatType;
+        this.damageType = damageType;
+        this.damage = damage;
+        this.speed = speed;
+        this.minStrength = minStrength;
     }
 
+
+
     public String getName() {
-        //implement this
+        return this.name;
     }
     
     public CombatType getCombatType() {
-        //implement this
+        return this.combatType;
     }
 
     public DamageType getDamageType() {
-        //implement this
+        return this.damageType;
     }
 
     public int getDamage() {
-        //implement this
+        return damage;
     }
 
     public int getSpeed() {
-        //implement this
+        return speed;
     }
 
     public int getMinStrength() {
-        //implement this
+        return minStrength;
     }
     
     public int getValue() {
-        //implement this
+        return value;
     }
     
     public void setValue(int value) {
-        //implement this
+        this.value = value;
     }
 
-    @Override
+@Override
     public int hashCode() {
-        //implement this
+        int hash = 7;
+        hash = 11 * hash + Objects.hashCode(this.name);
+        hash = 11 * hash + Objects.hashCode(this.combatType);
+        hash = 11 * hash + Objects.hashCode(this.damageType);
+        hash = 11 * hash + this.damage;
+        hash = 11 * hash + this.speed;
+        hash = 11 * hash + this.minStrength;
+        hash = 11 * hash + this.value;
+        return hash;
     }
 
     @Override
     public boolean equals(Object obj) {
-        //implement this
+        if(obj.equals(obj))
+        {
+            return true;
+        }
+        return false;
     }
 
     @Override
     public String toString() {
-        return "Weapon{" + "name=" + name + ", combatType=" + combatType + ", damageType=" + damageType + ", damage=" + damage + ", speed=" + speed + ", minStrength=" + minStrength + ", value=" + value + '}';
+        return "Weapon" + "name=" + name + ", combatType=" + combatType + ", damageType=" + damageType + ", damage=" + damage + ", speed=" + speed + ", minStrength=" + minStrength + ", value=" + value + '}';
+    }
+    
+    public void readFile(String line)
+    {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("weapons"));
+            while((line = br.readLine()) != null)
+            {
+                String [] parts = line.split(" ");
+                Weapon w = new Weapon(parts[0], CombatType.valueOf(parts[1]), DamageType.valueOf(parts[2]), Integer.parseInt(parts[3]), Integer.parseInt(parts[4]), Integer.parseInt(parts[5]));
+            }
+            br.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Weapon.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Weapon.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
